31cb66beb09bb7c0b8f74c825018db0f05c1bc0b
trevendevpractice <123999422+trevendevpractice@users.noreply.github.com>
2023-02-01 15:29:04 -0600

    Update streamlit_app.py
    moved stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,16 +43,6 @@ except URLError as e:
 streamlit.write('The user entered ', fruit_choice)
 
 
-
-
-streamlit.stop()
-
-
-
-
-
-
-
 streamlit.header("The fruit load list contains:")
 
 def get_fruit_load_list():
@@ -65,7 +55,11 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
+  
+streamlit.stop()
+
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
 
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
